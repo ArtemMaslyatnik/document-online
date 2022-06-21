8cee5441063c0cf234650771b0199b615bd14fdc
LykovAS <LykovAS@vscnote098.vesco.com.ua>
2022-06-21 06:35:52 +0300

    Sales invoice

---
+++ frontend/modules/SalesInvoice/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\modules\SalesInvoice;
+
+/**
+ * SalesInvoice module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'frontend\modules\SalesInvoice\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
